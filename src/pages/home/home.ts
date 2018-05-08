56f6f9064b6d3304d2ba08ea1a8e42f4b3c743d7
mkhumalo <mkhumalo@mhg.co.za>
2018-05-08 15:54:48 +0200

    Home Screen update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  menuItems = [
+    {
+      name: 'Depression'
+    },
+    {
+      name: 'Anxiety'
+    },
+    {
+      name: 'Eating Dissorders'
+    },
+    {
+      name: 'Trauma'
+    },
+    {
+      name: 'Relationships'
+    },
+    {
+      name: 'Family Issues'
+    },
+    {
+      name: 'Careers'
+    }
+  ];
+
   constructor(public navCtrl: NavController) {
 
+  }
+  menuSelected(menu) {
+    switch (menu.name) {
+      default: console.log(menu.name);
+        break;
+    }
   }
 
 }
